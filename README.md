f6adef9f06b5a767876a27db539ca7000afbc845
Juan Diego <juandiegomariscal@gmail.com>
2022-05-27 20:17:05 +0200

    feat: sustituye palabras hardcodeadas

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Cambia palabras
 Primera Kata en Keepcoding
+
+## Forma de uso
+
+cambia_palabras fichero palabra_original palabra_nueva
---
+++ cambia_palabras.py
@@ -0,0 +1,12 @@
+original = "tiza"
+nueva = "yesos"
+
+fichero = open("fichero.txt", "r") #abro
+texto = fichero.read() #leemos
+fichero.close() #cerramos
+
+texto_final = texto.replace(original, nueva)
+
+fichero = open("fichero.txt", "w")
+fichero.write(texto_final)
+fichero.close()
