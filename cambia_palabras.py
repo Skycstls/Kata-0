6c32d90c0d69cb6470b36cd552740f4ea14d880f
Juan Diego <juandiegomariscal@gmail.com>
2022-05-27 20:44:11 +0200

    feat: peticion de datos

--- cambia_palabras.py
+++ cambia_palabras.py
@@ -1,12 +1,13 @@
-original = "tizas"
-nueva = "yesos"
+original = input("Esribe la palabra que quieres buscar: ")
+nueva = input("Escribe la palabra por la que quieres sustituir: ")
+ruta = input("Escribe la ruta del fichero: ")
 
-fichero = open("fichero.txt", "r") #abro
+fichero = open(ruta, "r") #abro
 texto = fichero.read() #leemos
 fichero.close() #cerramos
 
 texto_final = texto.replace(original, nueva)
 
-fichero = open("fichero.txt", "w")
+fichero = open(ruta, "w")
 fichero.write(texto_final)
 fichero.close()
